3c7d341a64f1be1d1117b726864016180540d01a
mvadly <mfadly24@gmail.com>
2022-01-19 17:47:21 +0700

    update fix local and host

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 define("BASEPATH", __DIR__);
-require_once BASEPATH ."/helper/myHelper.php";
+require_once BASEPATH . "/helper/myHelper.php";
 require_once BASEPATH . "/vendor/autoload.php";
-require_once BASEPATH."/library/upload.php";
+require_once BASEPATH . "/library/upload.php";
 
 $upload = new Upload();
 $url = $_SERVER["REQUEST_URI"];
@@ -10,23 +10,34 @@ $uri = explode("/", parse_url($url, PHP_URL_PATH));
 $base_url = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . preg_replace('@/+$@', '', dirname($_SERVER['SCRIPT_NAME'])) . '/';
 $whitelist = whiteListIp();
 
-if (isset($uri[3])) {
-    switch ($uri[3]) {
+
+
+switch ($_SERVER['HTTP_HOST']) {
+    case "localhost":
+        $x = 3;
+        break;
+    default:
+        $x = 2;
+        break;
+}
+
+if (isset($uri[$x])) {
+    switch ($uri[$x]) {
         case "upload_file":
             if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
                 $upload->singleFile("image");
-            }else{
+            } else {
                 forbidden();
             }
             break;
         case "images":
             if (isset($uri[4]) && strlen($uri[4]) > 0) {
-                $file = __DIR__."/images/".$uri[4];
+                $file = __DIR__ . "/images/" . $uri[4];
                 if (file_exists($file)) {
-                    // header("location:".$base_url."/index.php/images/".$uri[4]);
-                    echo '<img src="'.$base_url."images/".$uri[4].'" />';
+                    header("location:" . $base_url . "images/" . $uri[4]);
+                    // echo '<img src="'.$base_url."images/".$uri[4].'" />';
                 }
-            }else{
+            } else {
                 notFound();
             }
 
@@ -35,6 +46,6 @@ if (isset($uri[3])) {
             notFound();
             break;
     }
-}else{
+} else {
     notFound();
 }
